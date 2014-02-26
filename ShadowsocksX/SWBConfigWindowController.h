333dff63cab6294ea05b9dde26f60636a915049e
clowwindy <clowwindy42@gmail.com>
2014-02-26 17:42:43 +0800

    add config window

---
+++ ShadowsocksX/SWBConfigWindowController.h
@@ -0,0 +1,10 @@
+//
+// Created by clowwindy on 14-2-26.
+// Copyright (c) 2014 clowwindy. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface SWBConfigWindowController : NSWindowController
+@end
