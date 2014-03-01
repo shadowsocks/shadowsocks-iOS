835c66a31068264e856620a4d84d4a70fdb1931b
clowwindy <clowwindy42@gmail.com>
2014-03-01 17:23:20 +0800

    fix key shortcuts

---
+++ ShadowsocksX/SWBApplication.h
@@ -0,0 +1,10 @@
+//
+// Created by clowwindy on 3/1/14.
+// Copyright (c) 2014 clowwindy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface SWBApplication : NSApplication
+@end
