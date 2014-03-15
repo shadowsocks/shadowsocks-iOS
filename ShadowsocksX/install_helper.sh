42657396afb23972188ea5d57fdd251f291bfc41
clowwindy <clowwindy42@gmail.com>
2014-03-15 18:11:21 +0800

    fix helper install script

--- ShadowsocksX/install_helper.sh
+++ ShadowsocksX/install_helper.sh
@@ -5,6 +5,7 @@
 #
 #  Created by clowwindy on 14-3-15.
 
+cd `dirname "${BASH_SOURCE[0]}"`
 sudo mkdir -p "/Library/Application Support/ShadowsocksX/"
 sudo cp shadowsocks_sysconf "/Library/Application Support/ShadowsocksX/"
 sudo chown root:admin "/Library/Application Support/ShadowsocksX/shadowsocks_sysconf"
