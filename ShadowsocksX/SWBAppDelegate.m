edbe269420271399343a986515b0e36dbbd6dbab
Charlie Qiu <qbowater@gmail.com>
2016-01-27 06:16:26 +0800

    Resupport add new server by scan QRCode.
    Scan QRCode by Core Image CIDetector.
    There is no need of lib ZXing.

--- ShadowsocksX/SWBAppDelegate.m
+++ ShadowsocksX/SWBAppDelegate.m
@@ -16,6 +16,7 @@
 #import "AFNetworking.h"
 
 #import "LaunchAtLoginController.h"
+#import <CoreImage/CoreImage.h>
 
 #define kShadowsocksIsRunningKey @"ShadowsocksIsRunning"
 #define kShadowsocksRunningModeKey @"ShadowsocksMode"
@@ -309,6 +310,70 @@ void onPACChange(
         [qrCodeWindowController.window makeKeyAndOrderFront:nil];
     } else {
         // TODO
+    }
+}
+
+- (void)scanQRCode {
+    {
+        /* displays[] Quartz display ID's */
+        CGDirectDisplayID   *displays = nil;
+        
+        CGError             err = CGDisplayNoErr;
+        CGDisplayCount      dspCount = 0;
+        
+        /* How many active displays do we have? */
+        err = CGGetActiveDisplayList(0, NULL, &dspCount);
+        
+        /* If we are getting an error here then their won't be much to display. */
+        if(err != CGDisplayNoErr)
+        {
+            NSLog(@"Could not get active display count (%d)\n", err);
+            return;
+        }
+        
+        /* Allocate enough memory to hold all the display IDs we have. */
+        displays = calloc((size_t)dspCount, sizeof(CGDirectDisplayID));
+        
+        // Get the list of active displays
+        err = CGGetActiveDisplayList(dspCount,
+                                     displays,
+                                     &dspCount);
+        
+        /* More error-checking here. */
+        if(err != CGDisplayNoErr)
+        {
+            NSLog(@"Could not get active display list (%d)\n", err);
+            return;
+        }
+        
+        NSMutableArray* foundSSUrls = [NSMutableArray array];
+        
+        CIDetector *detector = [CIDetector detectorOfType:@"CIDetectorTypeQRCode"
+                                                  context:nil
+                                                  options:@{ CIDetectorAccuracy:CIDetectorAccuracyHigh }];
+        
+        for (unsigned int displaysIndex = 0; displaysIndex < dspCount; displaysIndex++)
+        {
+            /* Make a snapshot image of the current display. */
+            CGImageRef image = CGDisplayCreateImage(displays[displaysIndex]);
+            NSArray *features = [detector featuresInImage:[CIImage imageWithCGImage:image]];
+            for (CIQRCodeFeature *feature in features) {
+                NSLog(@"%@", feature.messageString);
+                if ( [feature.messageString hasPrefix:@"ss://"] )
+                {
+                    [foundSSUrls addObject:[NSURL URLWithString:feature.messageString]];
+                }
+            }
+        }
+        
+        free(displays);
+        
+        // Add urls in foundSSUrls to settings.
+        for (NSURL* url in foundSSUrls) {
+            [ShadowsocksRunner openSSURL:url];
+        }
+        
+        [self updateServersMenu];
     }
 }
 
