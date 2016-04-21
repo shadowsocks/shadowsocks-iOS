dbdfb42aa4471f2fe54e4448d99fcfb2f16f3db4
s7lx <s7lxhacker@gmail.com>
2016-04-22 04:45:21 +0800

    Update SWBAppDelegate.m
    replace gfwlist from googlecode to github

--- ShadowsocksX/SWBAppDelegate.m
+++ ShadowsocksX/SWBAppDelegate.m
@@ -460,7 +460,7 @@ void onPACChange(
 }
 
 - (void)updatePACFromGFWList {
-    [manager GET:@"https://autoproxy-gfwlist.googlecode.com/svn/trunk/gfwlist.txt" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [manager GET:@"https://github.com/gfwlist/gfwlist/raw/master/gfwlist.txt" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
         // Objective-C is bullshit
         NSData *data = responseObject;
         NSString *str = [[NSString alloc] initWithData:data encoding:NSUTF8StringEncoding];
